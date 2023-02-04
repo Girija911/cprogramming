89206d71acfcfdd1a674d5c770aecfe518090471
Girija911 <122432707+Girija911@users.noreply.github.com>
2023-02-03 23:21:07 -0800

    Create N10.C

---
+++ N10.C
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+
+    double num;
+    printf("Enter a number: ");
+    scanf("%lf", &num);
+    if (num <= 0.0) {
+        if (num == 0.0)
+            printf("You entered 0.");
+        else
+            printf("You entered a negative number.");
+    } 
+    else
+        printf("You entered a positive number.");
+
+    return 0;
+}
+Run Code
+You can also solve this problem using nested if...else statement.
+
+Check Positive or Negative Using if...else Ladder
+#include <stdio.h>
+
+int main() {
+
+    double num;
+    printf("Enter a number: ");
+    scanf("%lf", &num);
+
+    if (num < 0.0)
+        printf("You entered a negative number.");
+    else if (num > 0.0)
+        printf("You entered a positive number.");
+    else
+        printf("You entered 0.");
+
+    return 0;
+}
+© Parewa Labs Pvt. Ltd. All rights reserved.
+
+   
