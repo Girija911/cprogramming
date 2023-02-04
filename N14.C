bffb0e44bbf59f11ff397d469a2b98ae93a6e3bd
Girija911 <122432707+Girija911@users.noreply.github.com>
2023-02-03 23:26:51 -0800

    Create N14.C

---
+++ N14.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+   float x = 10.327;
+   int y = 28;
+   printf("The float value : %f
+", x);
+   printf("The sum of float and int variable : %f
+", (x+y));
+   return 0;
+}
