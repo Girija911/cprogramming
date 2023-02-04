a445c2743d718ae252ae78d520b9abd9e6d90a4b
Girija911 <122432707+Girija911@users.noreply.github.com>
2023-02-03 23:27:56 -0800

    Create C15.C

---
+++ C15.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+   float x = 10.327;
+   double y = 4244.546;
+   int z = 28;
+   printf("The float value : %f
+", x);
+   printf("The double value : %f
+", y);
+   printf("The sum of float,
+
+   double and int variable : %f
+", (x+y+z));
+   return 0;
+}
